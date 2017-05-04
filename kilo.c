b290c6b686c8957dd3aa33c5e8eef9b4d27935a5
Sachleen Sandhu <sachleen.sandhu@gmail.com>
2017-05-04 16:08:30 -0700

    added comment

--- kilo.c
+++ kilo.c
@@ -4,17 +4,22 @@
 
 struct termios orig_termios;
 
-
+/*
+ * Turn ECHO on so characters are visible on the terminal.
+ */
 void disableRawMode() {
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
 }
 
+/*
+ * Turn ECHO off so characters are invisible on the terminal.
+ */
 void enableRawMode() {
     tcgetattr(STDIN_FILENO, &orig_termios);
     atexit(disableRawMode);
     
     struct termios raw = orig_termios;
-    raw.c_lflag &= ~(ECHO);
+    raw.c_lflag &= ~(ECHO | ICANON);
     
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
